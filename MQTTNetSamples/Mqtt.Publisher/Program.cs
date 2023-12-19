155ca7b79cc7e4c45ec8248786258703c684a7d5
Hacı Coşkun <hacicoskun07@gmail.com>
2023-12-19 22:32:18 +0300

    init

---
+++ MQTTNetSamples/Mqtt.Publisher/Program.cs
@@ -0,0 +1,27 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Protocol;
+
+var mqttFactory = new MqttFactory();
+var mqttClient = mqttFactory.CreateMqttClient();
+
+var options = new MqttClientOptionsBuilder()
+    .WithTcpServer("localhost", 1883)
+    .WithClientId("publisher_client")
+    .WithCredentials("admin", "Password123!")
+    .Build();
+
+await mqttClient.ConnectAsync(options);
+
+// Your publish logic here
+var message = new MqttApplicationMessageBuilder()
+    .WithTopic("values")
+    .WithPayload("test message")
+    .WithQualityOfServiceLevel(MqttQualityOfServiceLevel.ExactlyOnce)
+    .WithRetainFlag()
+    .Build();
+
+await mqttClient.PublishAsync(message);
+
+await mqttClient.DisconnectAsync();
+ 
---
+++ MQTTNetSamples/Mqtt.Subscriber/Program.cs
@@ -0,0 +1,46 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using System.Text;
+
+var mqttFactory = new MqttFactory();
+var mqttClient = mqttFactory.CreateMqttClient();
+var options = new MqttClientOptionsBuilder()
+.WithTcpServer("localhost", 1883)
+//Broker Ip adresi ve Portu
+.WithClientId("publisher_client")
+//Brokera yayın yapan veya dinleyen Client Kimliği.
+.WithCredentials("admin", "Password123!")
+//Brokera bağlanmak için kullanıcı adı ve şifre var ise bu alan gereklidir.
+.WithProtocolVersion(MQTTnet.Formatter.MqttProtocolVersion.V500)
+//Mqtt versiyonunu belirler
+.WithWillContentType("text/plain")
+// Will mesajı: Mqtt broker yanıt vermediği durumlarda
+// son mesaj olarak kabul edilir.
+.WithWillPayload("Mqtt sunucusuna bağlantı kesildi.")
+// Will mesajının içeriğini belirleme
+.WithWillQualityOfServiceLevel(MQTTnet.Protocol.MqttQualityOfServiceLevel.AtMostOnce)
+
+//AtMostOnce  (0): Mesaj, alıcıya en fazla bir kez iletilir. 
+//Bu seviye, mesajın en az bir kere alıcıya ulaşmasını garanti etmez.
+
+//AtLeastOnce (1): Mesaj, alıcıya en az bir kez ulaşır. 
+//Ancak, alıcı bir onay göndermediğinde, mesaj tekrar gönderilebilir.
+
+//ExactlyOnce (2): Mesaj, tam olarak bir kez alıcıya ulaşır. 
+//Bu seviye, bir mesajın sadece bir kez alıcıya teslim edilmesini garanti eder.
+.Build();
+
+mqttClient.ApplicationMessageReceivedAsync += e =>
+{
+    Console.WriteLine($"Received application message. Topic: {e.ApplicationMessage.Topic}, Payload: {Encoding.UTF8.GetString(e.ApplicationMessage.Payload)}");
+
+    return Task.CompletedTask;
+};
+
+await mqttClient.ConnectAsync(options);
+
+await mqttClient.SubscribeAsync("values");
+
+Console.ReadLine();
+
+await mqttClient.DisconnectAsync();
